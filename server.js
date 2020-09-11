d525f30d02df18a2e6bc8521ccdcc4500332ea13
Max Gerringer <max.gerringer@gmail.com>
2020-09-10 20:10:34 -0400

    updated js files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+let PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
+
+app.listen(PORT, function () {
+	console.log("App listening on PORT " + PORT);
+});
